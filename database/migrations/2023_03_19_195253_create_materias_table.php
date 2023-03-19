d0ac2b4085125c50f3a453eea2c2c5c244a33d14
Angel_Delgado <al02959951@tecmilenio.mx>
2023-03-19 15:01:45 -0500

    Primera versión

---
+++ database/migrations/2023_03_19_195253_create_materias_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('materias', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('asignatura');
+            $table->string('actividad_1');
+            $table->string('calificacion_1');
+            $table->string('actividad_2');
+            $table->string('calificacion_2');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('materias');
+    }
+};
